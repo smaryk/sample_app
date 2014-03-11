5cc4cd4ba581b2d308a0b78cc3a41ab96e555f1e
Sabla <smkerketta@gmail.com>
2014-03-11 13:28:46 +0000

    Friendly forwarding changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,9 +43,14 @@ class UsersController < ApplicationController
     params.require(:user).permit( :name, :email, :password,
                                  :password_confirmation )
   end
-
-  def signed_in_user
-    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  
+  # Before filters
+  
+  def signed_in_user    
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
   end 
   
   def correct_user
